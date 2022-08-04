36f3a5fd75c042df643a2b909084d660da5c0533
nicolas andrade <nicolasandrade328@gmail.com>
2022-08-04 00:07:02 -0300

    agregamos un state al componente Titulo. terminamos ejercicio 3

--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Titulo = (props) => {
+
+    const [fromChangeState,setfromChangeState] = useState('');
+
     return (
         <div>
-            <h1>Hello {props.agregado}!</h1>
+            <h1>Hello {props.agregado} {fromChangeState}!</h1>
+            <button onClick={() => setfromChangeState('(from changed state)')}>
+                <p>cambio de estado</p>
+            </button>
         </div>
     );
 };
